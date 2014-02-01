c27dd12c9c38737144c7989b58474c088cd0a3de
tomsmeding <hallo@tomsmeding.nl>
2014-02-01 08:56:46 +0100

    More interactiveness and code format
    Created some kind of form to enter username, password and school. Also
    removed some random spaces at ends of lines.

--- beta.php
+++ beta.php
@@ -1,39 +1,108 @@
 <?php
-//beta no api interface yet, just variables for now
-//if you make it you can commit!
-$username="gebruikersnaam"; 
-$password="wachtwoord"; 
+function encodeURIComponent($str) {
+    $revert = array('%21'=>'!', '%2A'=>'*', '%27'=>"'", '%28'=>'(', '%29'=>')');
+    return strtr(rawurlencode($str), $revert);
+}
+
+$this_file_name="mata-beta.php";
+if(array_key_exists("getschoolname",$_GET)){
+	$ch = curl_init();
+	curl_setopt ($ch, CURLOPT_URL, "https://schoolkiezer.magister.net/home/query?filter=".encodeURIComponent($_GET["name"]));
+	curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+	curl_setopt ($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6");
+	curl_setopt ($ch, CURLOPT_TIMEOUT, 60);
+	curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt ($ch, CURLOPT_REFERER, "https://schoolkiezer.magister.net/home/query");
+	$result=curl_exec($ch);
+	if($result[0]=="<")$result='[{"Licentie":"Fout!","Url":"fail..."}]';
+	die($result);
+}
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Mata-beta</title>
 <style>
 body{
 	font-family: Arial;
 }
-code{
+pre{
 	font-size: 10px;
 }
 </style>
+<script>
+<?php
+echo "var this_file_name=\"$this_file_name\";";
+?>
+function updateSchoolNameSelect(){
+	var namepart,xhr;
+	namepart=document.getElementById("schoolNamePart").value;
+	xhr=new XMLHttpRequest();
+	xhr.onreadystatechange = function(){
+		if (xhr.readyState == 4 && xhr.status == 200){
+			var select,response,i,elem;
+			select=document.getElementById("schoolNameSelect");
+			if(xhr.responseText=="")response=[];
+			else response=JSON&&JSON.parse(xhr.responseText)||eval(xhr.responseText); //Because we all love eval, right? Right?
+			select.innerHTML="";
+			for(i=0;i<response.length;i++){
+				elem=document.createElement("option");
+				elem.setAttribute("value",response[i]["Url"]);
+				elem.innerHTML=response[i]["Licentie"];
+				select.appendChild(elem);
+			}
+		}
+	}
+	xhr.open("GET",this_file_name+"?getschoolname=1&name="+encodeURIComponent(namepart),true);
+	xhr.send();
+}
+</script>
+</head>
+<body>
+<?php
+if(!array_key_exists("username",$_POST)){
+	?>
+	<form action=<?php echo '"'.$this_file_name.'"';?> method="post">
+	<input type="text" id="schoolNamePart" placeholder="Naam van de school" onkeypress="updateSchoolNameSelect();"> &rarr;
+	<select id="schoolNameSelect" name="schoolName"><option>-- Typ een deel van de naam --</option></select><br>
+	Gebruikersnaam: <input type="text" name="username" placeholder="Gebruikersnaam"><br>
+	Wachtwoord: <input type="password" name="password" placeholder="Wachtwoord"><br>
+	<input type="submit" value="Log in">
+	</form>
+	</body>
+	</html>
+	<?php
+	die();
+} else {
+	$username=$_POST["username"]; //TODO make this more secure and shit!!!
+	$password=$_POST["password"];
+	$urlname=$_POST["schoolName"];
+	echo "Mata-site is: ".$urlname."<br>";
+}
+?>
 <?php
 error_reporting(0);
-$login="https://mata-sgtongerlo.magister.net/api/sessie";
-$cookie="cookie.txt"; 
+$login="https://$urlname/api/sessie";
+$cookie=".cookie.txt";
 
-$postdata = "Gebruikersnaam=".$username."&Wachtwoord=".$password; 
+$postdata = "Gebruikersnaam=".$username."&Wachtwoord=".$password;
 
 
 
-$ch = curl_init(); 
-curl_setopt ($ch, CURLOPT_URL, $login); 
-curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
-curl_setopt ($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"); 
-curl_setopt ($ch, CURLOPT_TIMEOUT, 60); 
-curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, 1); 
-curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1); 
-curl_setopt ($ch, CURLOPT_COOKIEJAR, $cookie); 
-curl_setopt ($ch, CURLOPT_REFERER, $login); 
+$ch = curl_init();
+curl_setopt ($ch, CURLOPT_URL, $login);
+curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt ($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6");
+curl_setopt ($ch, CURLOPT_TIMEOUT, 60);
+curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt ($ch, CURLOPT_COOKIEJAR, $cookie);
+curl_setopt ($ch, CURLOPT_REFERER, $login);
 
-curl_setopt ($ch, CURLOPT_POSTFIELDS, $postdata); 
-curl_setopt ($ch, CURLOPT_POST, 1); 
-$result = curl_exec ($ch); 
+curl_setopt ($ch, CURLOPT_POSTFIELDS, $postdata);
+curl_setopt ($ch, CURLOPT_POST, 1);
+$result = curl_exec ($ch);
 
 
 $result = json_decode($result, true);
@@ -45,125 +114,56 @@ $msg = $result["Message"];
 curl_close($ch);
 
 echo("Welkom, $naam<br>Uw gebruikersId is: $id<br>Uw sessie is: $session<br>Bericht: $msg");
+ob_flush();
 ?>
 <br><hr>
 <?php
-$huiswerk="https://mata-sgtongerlo.magister.net/api/leerlingen/$id/huiswerk/huiswerk";
-$ref = "https://mata-sgtongerlo.magister.net/";
-$cookie="cookie.txt"; 
-$strCookie = "SESSION_ID=$session";
+$huiswerk="https://$urlname/api/leerlingen/$id/huiswerk/huiswerk";
+$ref="https://$urlname/";
+//$strCookie="SESSION_ID=$session";
 
-$ch = curl_init(); 
-curl_setopt ($ch, CURLOPT_URL, $huiswerk); 
-curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
-curl_setopt ($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"); 
-curl_setopt ($ch, CURLOPT_TIMEOUT, 60); 
-curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, 1); 
-curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);  
-curl_setopt ($ch, CURLOPT_COOKIEFILE, $cookie); 
-curl_setopt ($ch, CURLOPT_REFERER, $ref); 
+$ch = curl_init();
+curl_setopt ($ch, CURLOPT_URL, $huiswerk);
+curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt ($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6");
+curl_setopt ($ch, CURLOPT_TIMEOUT, 60);
+curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt ($ch, CURLOPT_COOKIEFILE, $cookie);
+curl_setopt ($ch, CURLOPT_REFERER, $ref);
 
-$result = curl_exec ($ch); 
+$result = curl_exec ($ch);
 $result = json_decode($result, true);
 ?>
-<code>
+<pre>
 <?php
 var_dump($result);
+ob_flush();
 ?>
-</code>
+</pre>
+<br><hr><br><br>
 <?php
-echo '<br><hr><br><br>';
+$days=array(1=>"Maandag",2=>"Dinsdag",3=>"Woensdag",4=>"Donderdag",5=>"Vrijdag");
 //var_dump($result["Items"]["1"]["Items"][0]);
-echo "Maandag:";
-foreach($result as $items){
-	foreach($items as $uur){
-		foreach($uur as $item){
-			foreach($item as $les){
-				$datum = $les["Datum"];
-				$datum = array_shift(explode('T', $datum));
-				$datum = strtotime($datum);
-				$datum = date('N', $datum);
-				if($datum == "1"){
-				echo "<br>";
-				echo ($les["Lesuur"].' - '.$les["VakAfkortingen"].' - '.$les['Inhoud']);
+foreach($days as $daynum=>$dayname){
+	echo "<b>$dayname</b><ul>";
+	foreach($result as $items){
+		foreach($items as $uur){
+			foreach($uur as $item){
+				foreach($item as $les){
+					$datum = date('N',strtotime(array_shift(explode('T',$les["Datum"]))));
+					if($datum == "$daynum"){
+						echo "<li>".$les["Lesuur"]." - ".$les["VakAfkortingen"]." - ".$les['Inhoud']."</li>";
+					}
 				}
 			}
 		}
 	}
-}
-echo "<br><br>";
-echo "Dinsdag:";
-foreach($result as $items){
-	foreach($items as $uur){
-		foreach($uur as $item){
-			foreach($item as $les){
-				$datum = $les["Datum"];
-				$datum = array_shift(explode('T', $datum));
-				$datum = strtotime($datum);
-				$datum = date('N', $datum);
-				if($datum == "2"){
-				echo "<br>";
-				echo ($les["Lesuur"].' - '.$les["VakAfkortingen"].' - '.$les['Inhoud']);
-				}
-			}
-		}
-	}
-}
-echo "<br><br>";
-echo "Woensdag:";
-foreach($result as $items){
-	foreach($items as $uur){
-		foreach($uur as $item){
-			foreach($item as $les){
-				$datum = $les["Datum"];
-				$datum = array_shift(explode('T', $datum));
-				$datum = strtotime($datum);
-				$datum = date('N', $datum);
-				if($datum == "3"){
-				echo "<br>";
-				echo ($les["Lesuur"].' - '.$les["VakAfkortingen"].' - '.$les['Inhoud']);
-				}
-			}
-		}
-	}
-}
-echo "<br><br>";
-echo "Donderdag:";
-foreach($result as $items){
-	foreach($items as $uur){
-		foreach($uur as $item){
-			foreach($item as $les){
-				$datum = $les["Datum"];
-				$datum = array_shift(explode('T', $datum));
-				$datum = strtotime($datum);
-				$datum = date('N', $datum);
-				if($datum == "4"){
-				echo "<br>";
-				echo ($les["Lesuur"].' - '.$les["VakAfkortingen"].' - '.$les['Inhoud']);
-				}
-			}
-		}
-	}
-}
-echo "<br><br>";
-echo "Vrijdag:";
-foreach($result as $items){
-	foreach($items as $uur){
-		foreach($uur as $item){
-			foreach($item as $les){
-				$datum = $les["Datum"];
-				$datum = array_shift(explode('T', $datum));
-				$datum = strtotime($datum);
-				$datum = date('N', $datum);
-				if($datum == "5"){
-				echo "<br>";
-				echo ($les["Lesuur"].' - '.$les["VakAfkortingen"].' - '.$les['Inhoud']);
-				}
-			}
-		}
-	}
+	echo "</ul>";
 }
 //echo($result["Items"]["0"]["Items"][0]["Lesuur"]);
 
 curl_close($ch);
 ?>
+</body>
+</html>
