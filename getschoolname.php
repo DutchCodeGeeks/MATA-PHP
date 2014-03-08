43c4cb9eb8282fa89b666fa8a0644bca91bf12bd
tkon99 <tkon99@gmail.com>
2014-03-08 15:16:22 +0100

    Delete getschoolname.php
    deleting pages not used in API version

--- getschoolname.php
+++
@@ -1,6 +0,0 @@
-<?php
-require "utils.php";
-$result=curlget("https://schoolkiezer.magister.net/home/query?filter=".encodeURIComponent($_GET["name"]));
-if($result[0]=="<")$result='[{"Licentie":"Er is een fout opgetreden","Url":"FAIL"}]';
-die($result);
-?>
