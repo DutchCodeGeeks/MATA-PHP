9950c5b9ba3c88d27cceb532bc8b525ef85934f7
tomsmeding <hallo@tomsmeding.nl>
2014-05-22 20:54:20 +0200

    Fixed MegaCookie's additions to api-example.php
    Great work MegaCookie — just some typo stuff

--- api-example.php
+++ api-example.php
@@ -26,13 +26,13 @@ var_dump(mataphp\getHomework($session,new DateTime("now"),new DateTime("now +7 d
 var_dump(mataphp\getStudyGuideList($session)); //Get All Study Guides
 
 //Study Guide Content example:
-	$StudyGuideId=421; //!!!*** IMPORTANT TO MAKE IT WORK: Change this Id to a correct StudyGuide Id. You could call one from the StudyGuidesList
+	$StudyGuideId=861; //!!!*** IMPORTANT TO MAKE IT WORK: Change this Id to a correct StudyGuide Id. You could call one from the StudyGuidesList
 	$StudyGuide = (mataphp\getStudyGuideContent($session,$StudyGuideId));
 	foreach($StudyGuide->content as $items){
-		echo('<div><h1'.$items->title.'</h1>');
+		echo('<div><h1>'.$items->title.'</h1>');
 		echo'<p>'.$items->content.'</p>';
 		foreach ($items->attachments as $attachmentItem) {
-			if($attachmentItem->type == 1){echo('Attachment <a href="attachment.php?url='.$attachmentItem->url.'&id='.$sessie->sessionId.'">'.$attachmentItem->title.'</a><br>');
+			if($attachmentItem->type == 1){echo('Attachment <a href="attachment.php?url='.$attachmentItem->url.'&id='.$session->sessionId.'">'.$attachmentItem->title.'</a><br>');
 			}elseif($attachmentItem->type == 2){echo('Assignment: <a href="'.$attachmentItem->url.'" target="_blank">'.$attachmentItem->title.'</a><br>');
 			}elseif($attachmentItem->type == 3){echo('URL Link: <a href="'.$attachmentItem->url.'" target="_blank">'.$attachmentItem->title.'</a><br>');
 			}else{echo('YouTube video: <a href="'.$attachmentItem->url.'" target="_blank">'.$attachmentItem->title.'</a><br>');
