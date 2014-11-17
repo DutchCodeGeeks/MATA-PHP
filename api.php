916d121b9dd10a42d927f2d5a91a9a53e99629a5
Jesse van der Velden <megacookie@riseup.net>
2014-11-17 16:03:06 +0100

    Remove SSL3

--- api.php
+++ api.php
@@ -171,7 +171,7 @@ class Mataphp{
 		curl_setopt($ch,CURLOPT_URL,$url);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,FALSE);
 		curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
-		curl_setopt($ch, CURLOPT_SSLVERSION, 3);
+		//curl_setopt($ch, CURLOPT_SSLVERSION, 3);
 		curl_setopt($ch,CURLOPT_USERAGENT,"Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6");
 		curl_setopt($ch,CURLOPT_TIMEOUT,60);
 		curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
